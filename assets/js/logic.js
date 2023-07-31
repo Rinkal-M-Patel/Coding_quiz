57cb7e1c07bddd8f94b76d03b1d17ec2218c6873
Rinkal <email@example.com>
2023-07-31 14:58:58 +0100

    select each card div by id and assign to variables

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,2 +1,16 @@
 import { questions } from "./questions.js";
 
+//select each card div by id and assign to variables
+const startCard = document.querySelector("#start-screen");
+const questionCard = document.querySelector("#questions");
+const scoreCard = document.querySelector("#end-screen");
+const leaderboardCard = document.querySelector("#leaderboard-card"); 
+
+//hide all cards
+function hideCards(){
+    startCard.setAttribute("hidden", true);
+    questionCard.setAttribute("hidden", true);
+    scoreCard.setAttribute("hidden", true);
+    leaderboardCard.setAttribute("hidden", true);
+}
+
