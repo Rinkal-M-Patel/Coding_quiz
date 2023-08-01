c9ce1cb5c2a8dfb8b05c598d0896f88209c02e58
Rinkal <email@example.com>
2023-08-01 17:36:59 +0100

    add event handler for clear score

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,3 +2,8 @@
 const highScoresDisplay = document.getElementById("highscores");
 const clearBtn = document.getElementById("clear");
 const noScore = document.getElementById("no-scores");
+
+clearBtn.addEventListener("click", () => {
+    localStorage.clear();    
+  });
+
