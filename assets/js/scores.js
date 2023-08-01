84c7eb919888ab57bce77ae055aa08e5bce6c887
Rinkal <email@example.com>
2023-08-01 17:57:38 +0100

    add palce holder for when no score display

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -3,11 +3,27 @@ const highScoresDisplay = document.getElementById("highscores");
 const clearBtn = document.getElementById("clear");
 const noScore = document.getElementById("no-scores");
 
+const placeHolder = "no highscores";
+
 clearBtn.addEventListener("click", () => {
-    localStorage.clear();    
+    localStorage.clear();
+    highScoresDisplay.innerHTML = "";
+    noScore.innerText = placeHolder;   
   });
 
   const highScores = JSON.parse(localStorage.getItem("highScores"));
+
+  
+if (!highScores) {
+    noScore.innerText = placeHolder;
+  }
+  
+  if (highScores) {
+    highScores.sort((a, b) => b.score - a.score);
+  }
+  
+  highScoresDisplay.innerHTML = "";
+  
 
 if (highScores) {
   highScores.forEach((score) => {
