28f84ad59ea8e2ced13f55505a478766780296ea
Rinkal <email@example.com>
2023-08-01 17:45:11 +0100

    check condition for display high score

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -7,3 +7,13 @@ clearBtn.addEventListener("click", () => {
     localStorage.clear();    
   });
 
+  const highScores = JSON.parse(localStorage.getItem("highScores"));
+
+if (highScores) {
+  highScores.forEach((score) => {
+    const liElement = document.createElement("li");
+    liElement.textContent = `${score.name}: ${score.score}`;
+    highScoresDisplay.appendChild(liElement);
+  });
+}
+
