3f76960d235ca9e9d1c95d298ea4067116b5de33
Rinkal <email@example.com>
2023-08-01 18:09:52 +0100

    add comment in all js files

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,7 @@
-
-// import the question set
+// Import the question set
 import { questions } from "./questions.js";
 
+// Get references to DOM elements
 const time = document.getElementById("time");
 const questionCard = document.getElementById("questions"); 
 const questionTitle = document.getElementById("question-title");
@@ -15,35 +15,34 @@ const startButton = document.getElementById("start");
 const submitScore = document.getElementById("submit");
 const inputElement = document.getElementById("initials");
 
-// Create audio elements for handling offscreen
+// Create audio elements for handling offscreen sounds
 const correct = new Audio("./assets/sfx/correct.wav");
 const incorrect = new Audio("./assets/sfx/incorrect.wav");
 
-
+// Event listeners
 startButton.addEventListener("click", () => {
-  // ! call the core gameplay function
+  // Call the core gameplay function when the "Start Quiz" button is clicked
   startGame();
 });
 
 choices.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
+    // Check the selected answer when the user clicks on a choice button
     checkAnswer(e);
   }
 });
 
 submitScore.addEventListener("click", () => {
-
-  //check for input
+  // Check for input and add the user's score and initials to the high scores
   if (!inputElement.value) {
     alert("Please enter your initials before pressing submit!");
     return;
   }
-  // add user score and initials to the high scores
   addHighScore({ name: inputElement.value, score: score });
   location.href = "./highscores.html";
 });
 
-// Initialise global variables
+// Initialize global variables
 let timer = 60;
 time.innerText = timer;
 let intervalId;
@@ -51,24 +50,13 @@ let score = 0;
 let wrongAns = 10;
 let currentIndex = 0;
 
-
 function startGame() {
-  // hide the start-screen
+  // Hide the start-screen and show the questionCard
   startScreen.classList.add("hide");
-  // show the questionCard
   questionCard.classList.remove("hide");
   playerFeedback.classList.remove("hide");
 
-  if (timer <= 0) {
-    // if true the game is over
-    clearInterval(intervalId);
-    time.innerText = "Game Over!";
-    endGame();
-    return;
-  }
-
-  
-  // this interval sets the core game timer
+  // Start the core game timer using an interval
   intervalId = setInterval(() => {
     if (timer > 0) {
       timer -= 1;
@@ -80,36 +68,33 @@ function startGame() {
     }
   }, 1000);
 
-  
+  // Display the first question
   displayQuestion(currentIndex);
 }
 
 function endGame() {
-  
+  // Hide the questionCard and playerFeedback, show the end-screen
   questionCard.classList.add("hide");
   playerFeedback.classList.add("hide");
-
   endScreen.classList.remove("hide");
 
-  
+  // Display the final score
   finalScore.innerText = score;
-
 }
 
 function displayQuestion(questionIndex) {
+  // Check if there are no more questions left
   if (currentIndex >= questions.length) {
- 
     clearInterval(intervalId);
     time.innerText = "Game Over";
     endGame();
     return;
   }
 
-  // add question to the questionTitle heading
+  // Display the current question and its choices
   questionTitle.innerText = questions[questionIndex].question;
-
   choices.innerHTML = "";
-    questions[questionIndex].options.forEach((choice, index) => {
+  questions[questionIndex].options.forEach((choice, index) => {
     const button = document.createElement("button");
     button.value = index;
     button.id = `${questions[questionIndex].id}-${index}`;
@@ -119,33 +104,29 @@ function displayQuestion(questionIndex) {
 }
 
 function checkAnswer(e) {
-  
-  if (
-    questions[currentIndex].correctAnswer.toString() === e.target.value
-  ) {
+  // Check if the selected answer is correct and update the score accordingly
+  if (questions[currentIndex].correctAnswer.toString() === e.target.value) {
     score += 10;
     playerFeedback.textContent = "Correct!";
     correct.play();
-   
   } else {
     subtractTime();
     playerFeedback.textContent = "Incorrect!";
     incorrect.play();
   }
 
+  // Move to the next question
   currentIndex++;
-
- 
   displayQuestion(currentIndex);
 }
 
 function subtractTime() {
+  // Subtract time from the timer for incorrect answers
   timer -= wrongAns;
 }
 
-
 function addHighScore(newScore) {
-  
+  // Add the new score to the high scores in local storage
   var highScoresJSON = localStorage.getItem("highScores");
   var highScores = highScoresJSON ? JSON.parse(highScoresJSON) : [];
 
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,30 +1,36 @@
-
+// Get references to DOM elements
 const highScoresDisplay = document.getElementById("highscores");
 const clearBtn = document.getElementById("clear");
 const noScore = document.getElementById("no-scores");
 
+// Placeholder text for when there are no highscores
 const placeHolder = "no highscores";
 
+// Event listener for the "Clear Highscores" button
 clearBtn.addEventListener("click", () => {
-    localStorage.clear();
-    highScoresDisplay.innerHTML = "";
-    noScore.innerText = placeHolder;   
-  });
-
-  const highScores = JSON.parse(localStorage.getItem("highScores"));
-
-  
-if (!highScores) {
-    noScore.innerText = placeHolder;
-  }
-  
-  if (highScores) {
-    highScores.sort((a, b) => b.score - a.score);
-  }
-  
+  // Clear the highscores from local storage and update the display
+  localStorage.clear();
   highScoresDisplay.innerHTML = "";
-  
+  noScore.innerText = placeHolder;
+});
 
+// Get the highscores from local storage and parse the JSON data
+const highScores = JSON.parse(localStorage.getItem("highScores"));
+
+// Check if there are no highscores, and display the placeholder text if true
+if (!highScores) {
+  noScore.innerText = placeHolder;
+}
+
+// Sort the highscores in descending order based on the score
+if (highScores) {
+  highScores.sort((a, b) => b.score - a.score);
+}
+
+// Clear the previous content of the highscores display element
+highScoresDisplay.innerHTML = "";
+
+// If there are highscores, display them in the highScoresDisplay element
 if (highScores) {
   highScores.forEach((score) => {
     const liElement = document.createElement("li");
@@ -32,4 +38,3 @@ if (highScores) {
     highScoresDisplay.appendChild(liElement);
   });
 }
-
