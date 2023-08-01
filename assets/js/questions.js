40815123cb06a09c71dc06a4c0692ced2bd18745
Rinkal <email@example.com>
2023-08-01 16:33:23 +0100

    add qustion array in question.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,41 +1,47 @@
+// This file contains an array of questions including associated multiple choice answers and a flag for which is the correct answer
 
 export const questions = [
-    {
-      questionText: "Commonly used data types DO NOT include:",
-      options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-      answer: "3. alerts",
-    },
-    {
-      questionText: "Arrays in JavaScript can be used to store ______.",
-      options: [
-        "1. numbers and strings",
-        "2. other arrays",
-        "3. booleans",
-        "4. all of the above",
-      ],
-      answer: "4. all of the above",
-    },
-    {
-      questionText:
-        "String values must be enclosed within _____ when being assigned to variables.",
-      options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
-      answer: "3. quotes",
-    },
-    {
-      questionText:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      options: [
-        "1. JavaScript",
-        "2. terminal/bash",
-        "3. for loops",
-        "4. console.log",
-      ],
-      answer: "4. console.log",
-    },
-    {
-      questionText:
-        "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
-      options: ["1. break", "2. stop", "3. halt", "4. exit"],
-      answer: "1. break",
-    },
-  ]; 
+  {
+    id: 1,
+    question:
+      "Commonly used data types DO NOT include",
+    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    correctAnswer: 2,
+  },
+  {
+    id: 2,
+    question: "Arrays in JavaScript can be used to store ______.",
+    options: [ "1. numbers and strings",
+    "2. other arrays",
+    "3. booleans",
+    "4. all of the above",],
+    correctAnswer: 3,
+  },
+  {
+    id: 3,
+    question:
+      "String values must be enclosed within _____ when being assigned to variables.",
+    options: [
+      "1. commas", "2. curly brackets", "3. quotes", "4. parentheses"
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 4,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: [ "1. JavaScript",
+    "2. terminal/bash",
+    "3. for loops",
+    "4. console.log",],
+    correctAnswer: 3,
+  },
+  {
+    id: 5,
+    question: "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+    options: [
+      "1. break", "2. stop", "3. halt", "4. exit"
+    ],
+    correctAnswer: 0,
+  },
+  
+];
